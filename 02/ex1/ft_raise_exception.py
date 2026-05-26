90c3c64af33ca54b7e7a2a7db35657c1511611e7
Pedro Coelho Malato <pecoelho@c1r9s12.42lisboa.com>
2026-05-26 20:13:34 +0100

    bora

---
+++ 02/ex1/ft_raise_exception.py
@@ -0,0 +1,20 @@
+def input_temperature(temp_str: str) -> int:
+    conv: int = int(temp_str)
+    print(f"Temperature is now {conv}°C\n")
+    return conv
+
+
+def test_temperature() -> None:
+    print("=== Garden Temperature ===\n")
+    print("Input data is '25'")
+    try:
+        input_temperature("25")
+        input_temperature("abc")
+    except ValueError as error:
+        print("Input data is '25'")
+        print("Caught input_temperature error:", error, "\n")
+    print("All tests completed - program didn't crash!")
+
+
+if __name__ == "__main__":
+    test_temperature()
