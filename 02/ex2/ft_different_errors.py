26aa4c1d380a871058aa0654ad1a30bc02c63080
pmalato <pedrocoelhomalato@gmail.com>
2026-06-02 10:31:18 +0100

    yay, vai outro

--- 02/ex2/ft_different_errors.py
+++ 02/ex2/ft_different_errors.py
@@ -1,33 +1,31 @@
 def garden_operations(operation_number: int) -> None:
-    if operation_number == 0:
-        raise ValueError(f"Testing operation {operation_number} ...\n")
-    elif operation_number == 1:
-        raise ZeroDivisionError(f"Testing operation {operation_number} ...\n")
-    elif operation_number == 2:
-        raise FileNotFoundError(f"Testing operation {operation_number} ...\n")
-    elif operation_number == 3:
-        raise TypeError(f"Testing operation {operation_number} ...\n")
-    else:
-        print("Operation completed successfully\n")
+    match operation_number:
+        case 0:
+            int("abc")
+        case 1:
+            print(1 / 0)
+        case 2:
+            t = open("/nao/ha/aqui/nada")
+            t.close()
+        case 3:
+            print(1 + "abc")  # type: ignore[operator]
+        case _:
+            print("Operation completed successfully\n")
 
 
 def test_operations() -> None:
-    try:
-        garden_operations(0)
-    except ValueError as error:
-        print("Caught ValueError:", error, "\n")
-    try:
-        garden_operations(1)
-    except ZeroDivisionError as error:
-        print("Caught ZeroDivisionError:", error, "\n")
-    try:
-        garden_operations(2)
-    except FileNotFoundError as error:
-        print("Caught FileNotFoundError:", error, "\n")
-    try:
-        garden_operations(3)
-    except TypeError as error:
-        print("Caught TypeError:", error, "\n")
+    for i in range(5):
+        print(f"Testing operation {i} ...")
+        try:
+            garden_operations(i)
+        except (
+            ValueError,
+            ZeroDivisionError,
+            TypeError,
+            FileNotFoundError
+        ) as error:
+            print(f"Caught {type(error).__name__}:", error)
+    print("All types tested successfully!")
 
 
 def main() -> None:
