04013f3639358abd49c6ed2b03c0bde0354157ff
pmalato <pedrocoelhomalato@gmail.com>
2026-06-03 17:04:56 +0100

    i was wrong, este esta a dar trabalho

--- 03/ex2/ft_coordinate_system.py
+++ 03/ex2/ft_coordinate_system.py
@@ -2,11 +2,32 @@ import math
 
 
 def get_player_position() -> None:
-    pass
+    phrase: str = "Enter new coordinates as floats in format 'x, y, z': "
+    coordinates: list
+    while True:
+        try:
+            temp: str = ""
+            coordinates = []
+            for x in input(phrase):
+                if x == " ":
+                    continue
+                elif x == ",":
+                    coordinates += [float(temp)]
+                    temp = ""
+                else:
+                    temp += x
+            coordinates += [float(temp)]
+            coordinates = [tuple(coordinates)]
+            break
+        except ValueError:
+            print("Invalid syntax")
 
 
 def main() -> None:
-    print("Game Coordinate System ===")
+    print("Game Coordinate System ===\n")
+    print("Get a first set of coordinates")
+    get_player_position()
+    print("done")
 
 
 if __name__ == "__main__":
