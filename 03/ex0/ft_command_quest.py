45064eb6219714e466075453a7cd094087ae3260
pmalato <pedrocoelhomalato@gmail.com>
2026-06-03 11:02:50 +0100

    ole

--- 03/ex0/ft_command_quest.py
+++ 03/ex0/ft_command_quest.py
@@ -2,8 +2,18 @@ import sys
 
 
 def main() -> None:
+    i: int = len(sys.argv)
+    n: int = 1
     print("=== Command Quest ===")
-    print(f"Program name: {}")
+    print(f"Program name: {sys.argv[0]}")
+    while n < (i):
+        print(f"Argument {n}: {sys.argv[n]}")
+        n += 1
+    if i == 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {i - 1}")
+    print(f"Total arguments: {i}")
 
 
 if __name__ == "__main__":
