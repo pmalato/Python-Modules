0ffc8b3b349958b050be5b4b21568ce417e41249
pmalato <pedrocoelhomalato@gmail.com>
2026-06-03 19:39:04 +0100

    quaseee

--- 03/ex2/ft_coordinate_system.py
+++ 03/ex2/ft_coordinate_system.py
@@ -1,31 +1,50 @@
 import math
 
 
-def get_player_position() -> None:
-    phrase: str = "Enter new coordinates as floats in format 'x, y, z': "
-    coordinates: list
+def get_player_position() -> tuple:
+    phrase: str = "Enter new input1 as format in 'x, y, z': "
+    input1: list
+    temp: str
+    count: int = 0
+    distance: float
     while True:
         try:
-            temp: str = ""
-            coordinates = []
+            temp = ""
+            input1 = []
+            count = 0
             for x in input(phrase):
                 if x == " ":
+                    temp = ""
                     continue
                 elif x == ",":
-                    coordinates += [float(temp)]
+                    input1 += [float(temp)]
+                    count += 1
                     temp = ""
                 else:
                     temp += x
-            coordinates += [float(temp)]
-            coordinates = [tuple(coordinates)]
+            input1 += [float(temp)]
+            count += 1
+            if count != 3:
+                raise ValueError
+            coordinates: tuple = tuple(input1)
             break
         except ValueError:
             print("Invalid syntax")
+    print(f"Tuple gathered: {input1}")
+    print(f"It includes: X = {input1[0]},"
+          f" Y = {input1[1]}, Z = {input1[2]}")
+    distance = math.sqrt(
+        (input1[0] - 0.0)**2 +
+        (input1[1] - 0.0)**2 +
+        (input1[0] - 0.0)**2
+        )
+    print(f"Distance to center: {distance}")
+    return coordinates
 
 
 def main() -> None:
-    print("Game Coordinate System ===\n")
-    print("Get a first set of coordinates")
+    print("=== Game Coordinate System ===")
+    print("\nGet a first set of coordinates")
     get_player_position()
     print("done")
 
