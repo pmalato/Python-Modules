d49627759727fb7120d6c6575b5736cdbd31fe7a
pmalato <pedrocoelhomalato@gmail.com>
2026-06-06 11:49:12 +0100

    better

--- 04/ex0/ft_ancient_text.py
+++ 04/ex0/ft_ancient_text.py
@@ -1,26 +1,23 @@
 import sys
-# import typing
 
 
 def main() -> None:
     args: int = len(sys.argv)
-    if args == 1:
+    if args != 2:
         print("Usage: ft_ancient_text.py <file>")
     else:
         print("=== Cyber Archives Recovery ===")
-        for x in sys.argv:
-            if x == sys.argv[0]:
-                continue
-            try:
-                print(f"Accessing file: '{x}'")
-                file = open(x)
-                print("---")
-                print(file.read())
-                print("---")
-                file.close()
-                print(f"File {x} is closed.")
-            except (FileNotFoundError, PermissionError) as error:
-                print(f"Error opening file {x}:", error)
+        x = sys.argv[1]
+        try:
+            print(f"Accessing file: '{x}'")
+            file = open(x)
+            print("---")
+            print(file.read())
+            print("---")
+            file.close()
+            print(f"File '{x}' is closed.")
+        except (FileNotFoundError, PermissionError) as error:
+            print(f"Error opening file {x}:", error)
 
 
 if __name__ == "__main__":
--- 04/ex1/ft_archieve_creation.py
+++ 04/ex1/ft_archieve_creation.py
@@ -1,8 +1,23 @@
-# import sys
+import sys
 
 
 def main() -> None:
-    print("=== Cyber Archives Recovery & Preservation ===")
+    args: int = len(sys.argv)
+    if args != 2:
+        print("Usage: ft_ancient_text.py <file>")
+    else:
+        print("=== Cyber Archives Recovery ===")
+        x = sys.argv[1]
+        try:
+            print(f"Accessing file: '{x}'")
+            file = open(x)
+            print("---")
+            print(file.read())
+            print("---")
+            file.close()
+            print(f"File '{x}' is closed.")
+        except (FileNotFoundError, PermissionError) as error:
+            print(f"Error opening file {x}:", error)
 
 
 if __name__ == "__main__":
