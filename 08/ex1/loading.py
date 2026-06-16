47003ad6a8de15e5d149c23c7fc2076e98fd12f0
Pedro Malato <pedrocoelhomalato@gmail.com>
2026-06-16 11:18:07 +0100

    poetry, agora explicar

--- 08/ex1/loading.py
+++ 08/ex1/loading.py
@@ -1,6 +1,3 @@
-import pandas as pd  # type: ignore
-import numpy as np  # type: ignore
-import matplotlib.pyplot as plt  # type: ignore
 from sys import prefix, base_prefix
 import importlib as imp
 from importlib.metadata import PackageNotFoundError
@@ -12,19 +9,31 @@ def check_modules(name: str) -> str | None:
     except ImportError:
         return None
     try:
-        version = module.metadata
+        version = imp.metadata()
         return f"{module}, {version}, 'unknown'"
     except PackageNotFoundError:
         return None
 
 
 def main() -> None:
+
     is_venv = prefix != base_prefix
     if (is_venv):
+        imp_error1 = check_modules("pandas")
+        imp_error2 = check_modules("numpy")
+        imp_error3 = check_modules("matplotlib")
+        if imp_error1:
+            print(imp_error1)
+        if imp_error2:
+            print(imp_error2)
+        if imp_error3:
+            print(imp_error3)
+        if imp_error1 | imp_error2 | imp_error3:
+            return
+        import pandas as pd  # type: ignore
+        import numpy as np  # type: ignore
+        import matplotlib.pyplot as plt  # type: ignore
         df = pd.DataFrame()
-        ar1 = np.array([3, 10])
-        ar2 = np.array([5, 7])
-        plt.plot(ar1, ar2)
         df["Nation"] = [
             "Earth Realm",
             "Air Nomads",
@@ -34,9 +43,7 @@ def main() -> None:
         df["Bender"] = [30, 13, 15, 65]
         df["Non-bender Integration"] = [True, True, False, False]
         print("\nLOADING STATUS: Loading programs...")
-        print("")
-        print("")
-        plt.show()
+
     else:
         print("You forgot to enter the venv, bruh")
 
