5fe8d5903728dbba9bb7791e1523cad66c626454
Pedro Malato <pedrocoelhomalato@gmail.com>
2026-06-21 16:44:28 +0100

    ex1 completooooo

--- 10/ex1/higher_magic.py
+++ 10/ex1/higher_magic.py
@@ -9,17 +9,23 @@ def spell_combiner(spell1: Callable, spell2: Callable) -> Callable:
 
 def power_amplifier(base_spell: Callable, multiplier: int) -> Callable:
     def spell(target: str, power: int) -> str:
-        ...
+        return base_spell(target, power*multiplier)
+    return spell
 
 
 def conditional_caster(condition: Callable, spell: Callable) -> Callable:
-    def spell(target: str, power: int) -> str:
-        ...
+    def inner_spell(target: str, power: int) -> str:
+        if condition(target, power):
+            return spell(target, power)
+        else:
+            return "Spell fizzled"
+    return inner_spell
 
 
 def spell_sequence(spells: list[Callable]) -> Callable:
     def spell(target: str, power: int) -> str:
-        ...
+        return [x(target, power) for x in spells]
+    return spell
 
 
 def main() -> None:
@@ -29,9 +35,26 @@ def main() -> None:
     def burn(target: str, power: int) -> str:
         return f"{target} burns for {power} seconds"
 
-    print("Testing spell_combiner ...")
+    def fake(target: int, power: str) -> str:
+        return ""
+
+    print("\nTesting spell_combiner ...")
     combine = spell_combiner(fireball, burn)
     print(combine("Dragon", 4))
+    print("\nTesting power_amplifier ...")
+    mega_fireball = power_amplifier(fireball, 3)
+    print(mega_fireball("Dragon", 15))
+    print("\nTesting conditional_caster ...")
+    attempt1 = conditional_caster(fake, burn)
+    print(attempt1(0, 30))
+    attempt2 = conditional_caster(fireball, burn)
+    print(attempt2("Dragon", 23))
+    print("\nTesting spell_combiner ...")
+    final = spell_sequence([
+        combine,
+        mega_fireball,
+        attempt2])
+    print(final("dragon", 30))
 
 
 if __name__ == "__main__":
